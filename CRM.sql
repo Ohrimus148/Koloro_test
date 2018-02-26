0ee8253ab31791615054751b7d16a9aa864fe1f3
Ohrimus148 <Ohrimus@yandex.ru>
2018-02-26 02:04:15 +0200

    nm

---
+++ CRM.sql
@@ -0,0 +1,245 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 26 2018 г., 02:58
+-- Версия сервера: 5.7.16
+-- Версия PHP: 7.0.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `CRM`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `managers`
+--
+
+CREATE TABLE `managers` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `photo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `company` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `managers`
+--
+
+INSERT INTO `managers` (`id`, `name`, `email`, `phone`, `photo`, `company`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Ira', 'Oleg.Okhrymenko@gmail.com', 967474301, '5a934994531ac.png', 'KFC', NULL, '2018-02-25 14:13:06', '2018-02-25 21:41:08'),
+(2, 'Misha', 'Misha@yandex.ru', 967474301, '5a9349a0c682d.png', 'KFC', NULL, '2018-02-25 14:27:34', '2018-02-25 21:41:20'),
+(3, 'Alex', 'alexx1984@ukr.net', 89878787, '5a9349abe32f8.jpg', 'MC', NULL, '2018-02-25 21:10:51', '2018-02-25 21:41:31'),
+(4, 'Max', 'Max@yandex.ru', 967454545, '5a9349b7477fc.png', 'MIT', NULL, '2018-02-25 21:11:58', '2018-02-25 21:41:43'),
+(5, 'Oleg', 'Ivan@gmail.com', 967474301, '5a9349e1344f6.png', 'KFC', NULL, '2018-02-25 21:42:25', '2018-02-25 21:42:25');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2018_02_25_084320_create_managers_table', 1),
+(4, '2018_02_25_084411_create_projects_table', 1),
+(5, '2018_02_25_090001_create_manager_project_pivot_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `projects`
+--
+
+CREATE TABLE `projects` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `performer` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `start` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `finish` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `manager_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `projects`
+--
+
+INSERT INTO `projects` (`id`, `name`, `price`, `performer`, `start`, `finish`, `manager_id`, `created_at`, `updated_at`) VALUES
+(1, 'KIT', '3456', 'Misha', '2018-02-09', '2018-02-24', NULL, '2018-02-25 15:38:59', '2018-02-25 15:38:59'),
+(2, 'MiT', '644664', 'Misha', '2018-02-08', '2018-02-16', NULL, '2018-02-25 15:39:36', '2018-02-25 20:54:58'),
+(3, 'KRIT', '23', 'Ol', '2018-02-23', '2018-02-28', NULL, '2018-02-25 20:12:38', '2018-02-25 20:55:26'),
+(4, 'KIT', '3456', 'Misha', '2018-02-09', '2018-02-24', NULL, '2018-02-25 20:47:25', '2018-02-25 20:47:25');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `project_manager`
+--
+
+CREATE TABLE `project_manager` (
+  `project_id` int(10) UNSIGNED NOT NULL,
+  `manager_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `project_manager`
+--
+
+INSERT INTO `project_manager` (`project_id`, `manager_id`) VALUES
+(1, 1),
+(1, 2),
+(1, 4),
+(2, 1),
+(2, 2),
+(2, 3),
+(2, 4),
+(4, 3),
+(4, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Oleg', 'Oleg.Okhrymenko@gmail.com', '$2y$10$FOUNSTu38tWJBGNIx9tHxuTDdd3HcIxccPxlK7sIHCouPRcu7BTb2', 'dCQJ9lUF1bRRBPq4avs16CDe5XrvKuCMIF4nHbQKPZJnNqEhJrJab1X5Lj7g', '2018-02-25 15:43:38', '2018-02-25 15:43:38');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `managers`
+--
+ALTER TABLE `managers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `managers_email_unique` (`email`);
+
+--
+-- Индексы таблицы `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Индексы таблицы `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `project_manager`
+--
+ALTER TABLE `project_manager`
+  ADD PRIMARY KEY (`project_id`,`manager_id`),
+  ADD KEY `project_manager_project_id_index` (`project_id`),
+  ADD KEY `project_manager_manager_id_index` (`manager_id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `managers`
+--
+ALTER TABLE `managers`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT для таблицы `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT для таблицы `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `project_manager`
+--
+ALTER TABLE `project_manager`
+  ADD CONSTRAINT `project_manager_manager_id_foreign` FOREIGN KEY (`manager_id`) REFERENCES `managers` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `project_manager_project_id_foreign` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
